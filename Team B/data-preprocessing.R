cfe997423b7256f2f252239d842595efeb147487
Victor-Thrive <victorthrive4light@gmail.com>
2025-02-25 18:32:57 +0000

    dashboard first row

--- Team B/data-preprocessing.R
+++ Team B/data-preprocessing.R
@@ -61,11 +61,7 @@ risk_levels <- new_df |>
     summarise(churn_rate = mean(churn_status == "churned") * 100) |>
     arrange(desc(churn_rate))
   
-  # total numbers of customers
-  new_df |>
-    count(acct_id) |> 
-    nrow()
-  
+ 
   #first 10 observations (rows)
   new_df |>
     count(acct_id) |>
